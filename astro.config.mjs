f93673f4c0622179345cb73549ea2056ecc45428
Luka <lukawolf.dev@gmail.com>
2025-02-18 03:47:15 -0300

    Fixed PWA and SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,6 +18,25 @@ export default defineConfig({
         type: "module",
       },
       registerType: "autoUpdate",
+      manifest: {
+        name: "E-commerce",
+        short_name: "e-commerce",
+        icons: [
+          {
+            src: "/ecommerce/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/ecommerce/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        theme_color: "#db6b42",
+        background_color: "#db6b42",
+        display: "standalone",
+      },
     }),
   ],
   vite: {
