d2e179b8bdc3688a020257c1157a859c0b344de4
Luka <lukawolf.dev@gmail.com>
2025-04-07 15:01:03 -0300

    Theme button fixed

--- src/components/atoms/ThemeModeButton.tsx
+++ src/components/atoms/ThemeModeButton.tsx
@@ -3,20 +3,36 @@ import { useEffect, useState } from "react";
 import { Button } from "../ui/button";
 
 export function ThemeModeButton() {
-  const [theme, setThemeState] = useState<"theme-light" | "dark">(
-    "theme-light"
-  );
+  const [theme, setThemeState] = useState<"theme-light" | "dark">(() => {
+    // Try to get the current theme from the navigator not using localstorage.
+    // Create a function to check if the user prefers dark mode
+    if (
+      document.documentElement.classList.contains("dark") ||
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      return "dark";
+    }
+
+    return "theme-light";
+
+    // Initialize theme based on the current DOM state
+
+    // return document.documentElement.classList.contains("dark")
+    //   ? "dark"
+    //   : "theme-light";
+  });
 
   useEffect(() => {
-    const isDarkMode = document.documentElement.classList.contains("dark");
-    setThemeState(isDarkMode ? "dark" : "theme-light");
-  }, []);
+    if (typeof window === "undefined") {
+      return;
+    }
 
-  useEffect(() => {
-    const isDark =
-      theme === "dark" ||
-      window.matchMedia("(prefers-color-scheme: dark)").matches;
-    document.documentElement.classList[isDark ? "add" : "remove"]("dark");
+    // Synchronize the DOM class with the theme state
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
   }, [theme]);
 
   return (
